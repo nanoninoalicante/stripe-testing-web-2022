407d8281f46a75218dff79f0a1bdb342927df9c1
Christopher Hill <cjameshill@gmail.com>
2022-08-16 14:28:28 +0000

    Update 5 files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+<script setup>
+const stripe = window.Stripe('pk_test_51J18RHDDF4yhC66hWTwc5VDTXvaFb6rDsZre0vI8JlX49yHahnkKwmRgvONk8OD7BlpJzuewwTL7ww8581FB7G3Y00TZ2X7ezB');
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="w-full flex justify-center items-center bg-white md:w-2/3 py-8 rounded-full">
+    <div>
+      <h1 class="text-xl font-medium">Welcome</h1>
+    </div>
+  </section>
 </template>
